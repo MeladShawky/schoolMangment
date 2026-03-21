436af70a4970225b47b3a9902e89d374a3133ebc
melad shawky <meladshawky577@gmail.com>
2026-03-21 17:45:12 +0200

    add validation to StudentDto and handle validation errors in StudentController

--- app/src/main/java/com/melo/app/student/StudentController.java
+++ app/src/main/java/com/melo/app/student/StudentController.java
@@ -1,12 +1,20 @@
 package com.melo.app.student;
+import java.util.HashMap;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 
 
@@ -22,7 +30,7 @@ public class StudentController {
     ////////////////////////////////////////////////////////////
     @PostMapping("/students")
     public StudentResponseDto saveStudent(
-       @RequestBody StudentDto studentDto
+       @Valid @RequestBody StudentDto studentDto
     ){
         return this.studentService.saveStudent(studentDto);
     }
@@ -54,4 +62,19 @@ public class StudentController {
     ){
         studentService.del(d);
     }
+
+    /////////////////////////////////////////////////////////////
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException ex
+    ) {
+        var errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            var fieldName = ((FieldError) error).getField();
+            var errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    }
+    
 }
--- app/src/main/java/com/melo/app/student/StudentDto.java
+++ app/src/main/java/com/melo/app/student/StudentDto.java
@@ -1,7 +1,11 @@
 package com.melo.app.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+     @NotEmpty(message = "First name should not be empty")
      String firstName,
+     @NotEmpty(message = "Last name should not be empty")
      String lastName,
      String email,
      Integer schoolId
